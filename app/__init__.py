1aaf1a4b8b7c387dd9d7707c644e1fabc79321ff
Lucas Sousa <lucasssousa10@gmail.com>
2020-05-22 00:47:35 -0300

    dados

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,40 @@ from flask import Flask, request, jsonify
 from flask import render_template
 from flask_script import Manager
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 import sys
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 
 CORS(app)
+app.config.from_object('config')
+db  = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+class Registers(db.Model):
+    __tablename__ = "registers"
+
+    id = db.Column(db.BigInteger, primary_key=True)
+    ip = db.Column(db.String(255), nullable=True)
+    x_coord = db.Column(db.String(255), nullable=True)
+    y_coord = db.Column(db.String(255), nullable=True)
+
+    # --------------------------------------------------------------------------------------------------#
+
+    def __init__(self, ip, x_coord, y_coord):
+        self.ip = ip
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+
+    # --------------------------------------------------------------------------------------------------#
+
+    def __repr__(self):
+        return "<register %r>" % self.ip
+
 
 @app.route('/')
 def initial_page():
@@ -18,12 +46,9 @@ def receive_data():
     data = request.get_json()
     print("X: {}, Y: {}, IP: {}\n".format(data['x_coord'], data['y_coord'], data['ip']))
     sys.stdout.flush()
-    
-    fo= open("results.txt", "a")
-    filebuffer = ["X: {}, Y: {}, IP: {}\n".format(data['x_coord'], data['y_coord'], data['ip'])]
-    fo.writelines(filebuffer)
-    fo.close()
 
-    
-    
+    obj = Registers(ip=data['ip'], x_coord=data['x_coord'], y_coord=data['y_coord'])
+    db.session.add(obj)
+    db.session.commit()    
+        
     return jsonify({"success": True})
---
+++ config.py
@@ -0,0 +1,6 @@
+DEBUG = True
+
+#--------------------------------------------------------------------------------------------------#
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://rdmartldzdzrbw:062fb9d1f10bb0ab1bd73a98ddb3eb86a2f5f44b52a420bd076809e26375efeb@ec2-35-169-254-43.compute-1.amazonaws.com/dc6i5qjkiagqud'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
